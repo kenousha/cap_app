ea43c3efe40816f96cf34fd5fd22b1052aef2a9f
kenousha <154089171+kenousha@users.noreply.github.com>
2024-09-04 03:59:26 -0400

    redownloaded server, edited app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from './Components/Navbar/Navbar';
+import Login from './Components/Login/Login';
+import Sign_Up from './Components/Sign_Up/Sign_Up';
+import LandingPage from './Components/LandingPage/LandingPage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <BrowserRouter>
+          <Navbar/>
+            <Routes>
+                <Route path="/LandingPage" element={<LandingPage/>}/>
+                <Route path="/Login" element={<Login/>}/>
+                <Route path="/Sign_Up" element={<Sign_Up/>}/>
+            </Routes>
+        </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
