362520d1b8e79f68a74f2815240461e7a1bf439e
kenousha <154089171+kenousha@users.noreply.github.com>
2024-09-04 04:03:44 -0400

    new host in db.js

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:0F9bYcsnOTGRsC88WNVKrVTS@172.21.254.80:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
