7ad42be0db3f4af9251e850eef1b1ba6bd05769a
kenousha <154089171+kenousha@users.noreply.github.com>
2024-10-06 09:58:50 -0400

    edit notification

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:RnPT4XbEk6RZAs8PmT7Lwf4z@172.21.78.173:27017";
+const mongoURI =  "mongodb://root:fzct3EgAkE8xlWmIMAkEPVvd@172.21.101.50:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
