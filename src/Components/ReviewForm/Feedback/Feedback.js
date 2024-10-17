883657351d216e6448c70ee7f318462ff5cb3e65
kenousha <154089171+kenousha@users.noreply.github.com>
2024-10-17 12:36:07 -0400

    edit FINALLY

--- src/Components/ReviewForm/Feedback/Feedback.js
+++ src/Components/ReviewForm/Feedback/Feedback.js
@@ -1,40 +1,45 @@
 import React, { useState } from "react";
 
-function Feedback ({name, review, rating}) {
+function Feedback ({name, review, onSubmit}) {
 const [showWarning, setShowWarning] = useState(false);
 const handleFormSubmit = (e) => {
       e.preventDefault();
+      const reviewData = {
+        "name": name,
+        "review": review,
+      }
       const formData = new FormData (e.currentTarget);
-      const name = formData.get('name')
-      const review = formData.get('review')
-      const rating = formData.get('rating')
-      if (name.length && review.length && rating?.length > 0) {
+      const rating = formData.get("rating");
+      if (reviewData.name && reviewData.review && rating?.length > 0 ) { 
         setShowWarning(false);
       } else {
         setShowWarning(true);
       }
+      onSubmit({ name, review});
+
     };
+
 return (
 <form className="feedbackForm" onSubmit={handleFormSubmit} >
 {showWarning && <p className="warning">Please fill out all fields.</p>}
     <div className="form-group">
       <label htmlFor="name">Name:</label>
-      <input type="text" name="name"/>
+      <input type="text" id="name" value={name} onChange={(e) => (name= e.target.value)}/>
     </div>
                                     
     <div>
-        <label >Review:</label>
-        <textarea type="text" name="review"/>
+        <label htmlFor="review">Review:</label>
+        <textarea type="text" id="review" value={review} onChange={(e) => (review= e.target.value)}/>
     </div>
 
     <div className='rating'>
         <label >Rating:</label>
         <div className='starrating'>
-            <input type='radio' name="rating" id='star5'/> <label for='star5'  className="fa fa-star star"/>
-            <input type='radio' name="rating" id='star4'/> <label for='star4' className="fa fa-star star"/>
-            <input type='radio' name="rating" id='star3'/> <label for='star3' className="fa fa-star star"/>
-            <input type='radio' name="rating" id='star2'/> <label for='star2' className="fa fa-star star"/>
-            <input type='radio' name="rating" id='star1'/> <label for='star1' className="fa fa-star star"/>
+            <input type='radio' name="rating" id='star5'/> <label htmlFor='star5'  className="fa fa-star star"/>
+            <input type='radio' name="rating" id='star4'/> <label htmlFor='star4' className="fa fa-star star"/>
+            <input type='radio' name="rating" id='star3'/> <label htmlFor='star3' className="fa fa-star star"/>
+            <input type='radio' name="rating" id='star2'/> <label htmlFor='star2' className="fa fa-star star"/>
+            <input type='radio' name="rating" id='star1'/> <label htmlFor='star1' className="fa fa-star star"/>
         </div>
     </div>
     <button type="submit" >Submit</button>
