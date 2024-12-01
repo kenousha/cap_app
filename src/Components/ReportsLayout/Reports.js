612b946b62c3134fa6a78fd7c9d8ed24d650e1a9
kenousha <154089171+kenousha@users.noreply.github.com>
2024-12-01 12:16:30 -0500

    edit reports

--- src/Components/ReportsLayout/Reports.js
+++ src/Components/ReportsLayout/Reports.js
@@ -1,58 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import Popup from 'reactjs-popup';
+import React from 'react';
 import 'reactjs-popup/dist/index.css';
-import { v4 as uuidv4 } from 'uuid';
 import './Reports.css';
 import Report1 from './/Reports/Report1.pdf'; import Report2 from './/Reports/Report2.pdf';
 import Report3 from './/Reports/Report3.pdf'; import Report4 from './/Reports/Report4.pdf';
 
 const Reports = () => {
-
-    const getDoctorReport = (name) => {
-        switch (name) {
-          case 'Dr. John Doe': return Report1;    
-          case 'Dr. Michael Brown': return Report3;
-          case 'Dr. Emily Johnson': return Report2;    
-          case 'Dr. Sarah Davis': return Report4;
-                default:
-        return null;
-        }
+ const getDoctorReport = (name) => {
+     switch (name) {
+       case 'Dr. John Doe': return Report1;    
+       case 'Dr. Michael Brown': return Report3;
+       case 'Dr. Emily Johnson': return Report2;    
+       case 'Dr. Sarah Davis': return Report4;
+             default:
+     return null;
+     }
   };
-
-    const [showModal, setShowModal] = useState(false);
-    const [reviewedDoctor, setReviewedDoctor] = useState([]);
-    const [reports, setreports] = useState([]);
-    const [reviewData, setReviewData] = useState([]);
     
-    useEffect(() => {
-    const storedReviewData = ('reviewData');
-    if (storedReviewData) {
-        setReviewData(storedReviewData);
-      }
-    }, [reviewData]); 
-    
-    const handleReview = (doctor) => {
-      setReviewedDoctor(doctor);
-      setShowModal(true);
-    };
-  
-    const handleFormSubmit = async (reviewData) => {
-      const newReview = {
-        id: uuidv4(),
-        ...reviewData,
-        reviewedID: reviewedDoctor.number,
-      };
-      const updatedreports = [...reports, newReview];
-      setreports(updatedreports);
-      setShowModal(false);
-    };
-
-        const doctors = [
-            { number: 1, name: 'Dr. John Doe', speciality: 'Cardiology', feedback: 'Yes', review: '' },
-            { number: 2, name: 'Dr. Emily Johnson', speciality: 'Neurology', feedback: 'Yes', review: '' },
-            { number: 3, name: 'Dr. Michael Brown', speciality: 'Pediatrics', feedback: 'Yes', review: '' },
-            { number: 4, name: 'Dr. Sarah Davis', speciality: 'Oncology', feedback: 'No', review: '' },
-        ];
+ const doctors = [
+     { number: 1, name: 'Dr. John Doe', speciality: 'Cardiology'},
+     { number: 2, name: 'Dr. Emily Johnson', speciality: 'Neurology'},
+     { number: 3, name: 'Dr. Michael Brown', speciality: 'Pediatrics'},
+     { number: 4, name: 'Dr. Sarah Davis', speciality: 'Oncology'}
+     ];
 
 return (
  <div className="container" style={{overflow:'hidden'}}>
@@ -75,13 +44,13 @@ return (
          <td> {doctor.name} </td>
          <td>{doctor.speciality}</td>
          <td>
-          <button className='review-btn' onClick={() => handleReview(doctor)}>
+          <button className='review-btn'>
           <a target='_blank' href={getDoctorReport(doctor.name)} className='review-btn' rel="noreferrer" style={{color:'white'}}>View Report</a>
           </button>
 
          </td>
          <td>
-          <button className='review-btn' onClick={() => handleReview(doctor)}>
+          <button className='review-btn'>
           <a target='_blank' href={getDoctorReport(doctor.name)} download={getDoctorReport(doctor.name)} className='review-btn' rel="noreferrer" style={{color:'white'}}>Download Report</a>
           </button>
 
